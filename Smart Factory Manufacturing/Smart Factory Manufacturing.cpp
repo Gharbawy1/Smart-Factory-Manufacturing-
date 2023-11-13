145ebc5f83d50b076dbe362e0cf8d0ac9b939157
Ahmed El-gharbawy <136514345+Gharbawy1@users.noreply.github.com>
2023-11-13 05:03:48 +0200

    Add project files.

---
+++ Smart Factory Manufacturing/Smart Factory Manufacturing.cpp
@@ -0,0 +1,11 @@
+// Smart Factory Manufacturing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
