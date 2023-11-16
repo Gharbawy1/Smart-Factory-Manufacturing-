77d79659eb272c6e40291a85daa7140ab2a9eb20
Ahmed El-gharbawy <136514345+Gharbawy1@users.noreply.github.com>
2023-11-16 04:18:56 +0200

    Implment Enqueue & isempty functions

--- Smart Factory Manufacturing/Queue.cpp
+++ Smart Factory Manufacturing/Queue.cpp
@@ -1,1 +1,18 @@
 #include "Queue.h"
+using namespace std;
+template<class T>
+bool Queue<T>::isEmpty() {
+	return this->Front == NULL;
+}
+
+
+template<class T>
+void Queue<T>::Enqueue(T Value) {
+	if (isEmpty()) {
+		// is null make front  = last  = the first node
+		Front = last = new QNode;
+	}
+	last->next = new QNode;
+}
+
+
