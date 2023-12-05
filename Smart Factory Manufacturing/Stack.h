f6d52fdf871734d325f9fcd58f6a6c531cf6b4cd
Ahmed Nasr <133595087+AHmeDNasR54@users.noreply.github.com>
2023-12-05 04:50:24 +0200

    Update Stack.h

--- Smart Factory Manufacturing/Stack.h
+++ Smart Factory Manufacturing/Stack.h
@@ -1,5 +1,28 @@
-#pragma once
+#ifndef STACK_H
+#define STACK_H
+#include<iostream>
+#include"Operation.h"
+using namespace std;
+
 class Stack
 {
+private:
+    int top;
+    int size;
+    Operation** arr;
+
+public:
+    Stack();
+    Stack(int size);
+
+    void push(Operation* op);
+    Operation* pop();
+    Operation* stackTop();
+    void displayOperations();
+    virtual ~Stack();
+
+protected:
+
 };
 
+#endif // STACK_H
