0828549ea6d5fabebc94eddbb806138eecebee10
Ahmed Nasr <133595087+AHmeDNasR54@users.noreply.github.com>
2023-12-05 04:54:26 +0200

    Update Stack.cpp

--- Smart Factory Manufacturing/Stack.cpp
+++ Smart Factory Manufacturing/Stack.cpp
@@ -1,1 +1,63 @@
 #include "Stack.h"
+
+Stack::Stack()
+{
+    size = 5;
+    arr = new Operation * [size];
+    top = -1;
+}
+
+Stack::~Stack()
+{
+    for (int i = 0; i <= top; i++)
+        delete[]arr[i];
+    delete[]arr;
+}
+Stack::Stack(int size)
+{
+    this->size = size;
+    arr = new Operation * [size];
+    top = -1;
+
+
+}
+void Stack::push(Operation* op)
+{
+    if (top == size - 1)
+    {
+        cout << "Stack is full\n";
+        return;
+    }
+    arr[++top] = op;
+
+}
+Operation* Stack::pop()
+{
+    if (top == -1) {
+        cout << "Stack is empty\n";
+        return nullptr;
+    }
+    return arr[top--];
+
+}
+Operation* Stack::stackTop()
+{
+    if (top == -1) {
+        cout << "Stack is empty\n";
+        return nullptr;
+    }
+    return arr[top];
+
+}
+void Stack::displayOperations()
+{
+    if (top == -1)
+        cout << "Stack is empty\n";
+    else
+    {
+        for (int i = top;i >= 0;i--)
+            cout << *arr[i];
+
+    }
+
+}
