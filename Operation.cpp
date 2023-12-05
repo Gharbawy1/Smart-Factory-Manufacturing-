c4d2944c0a4e29cf83a482cab434a48851ca509c
Ahmed Nasr <133595087+AHmeDNasR54@users.noreply.github.com>
2023-12-05 04:57:18 +0200

    Create Operation.cpp

---
+++ Operation.cpp
@@ -0,0 +1,23 @@
+#include "Operation.h"
+
+Operation::Operation()
+{
+    operationName = "No name";
+}
+
+Operation::~Operation()
+{
+    //dtor
+}
+Operation::Operation(string name)
+{
+    operationName = name;
+}
+void Operation::setName(string name)
+{
+    operationName = name;
+}
+string Operation::getName()
+{
+    return operationName;
+}
