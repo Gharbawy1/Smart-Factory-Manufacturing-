51d576207b9f3fada7d04fa88ee172df301186b8
Ahmed Nasr <133595087+AHmeDNasR54@users.noreply.github.com>
2023-12-07 20:07:06 +0200

    Add files via upload
    Signed-off-by: Ahmed Nasr <133595087+AHmeDNasR54@users.noreply.github.com>

---
+++ Smart Factory Manufacturing/DistributionOperation.h
@@ -0,0 +1,31 @@
+#ifndef DISTRIBUTIONOPERATION_H
+#define DISTRIBUTIONOPERATION_H
+
+#include <Operation.h>
+
+
+class DistributionOperation : public Operation
+{
+    public:
+        DistributionOperation()
+        {
+            operationName="Distribution Operation";
+        }
+        virtual ~DistributionOperation()
+        {
+
+        }
+        void setName()
+        {
+            operationName="Distribution Operation";
+        }
+                string getName()
+        {
+            return operationName;
+        }
+    protected:
+
+    private:
+};
+
+#endif // DISTRIBUTIONOPERATION_H
